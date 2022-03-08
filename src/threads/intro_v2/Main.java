87194c4ac31d4eca9b284603a50fa2cdd2147895
alessandro <martinellialessandro04@gmail.com>
2022-03-08 11:11:33 +0100

    Thread Es3

---
+++ src/threads/intro_v2/Main.java
@@ -0,0 +1,10 @@
+package threads.intro_v2;
+
+public class Main {
+    public static int count = 0;
+
+    public static void main(String[] args) {
+        new Thread(new IncrementOf(1)).start();
+        new Thread(new IncrementOf(-1)).start();
+    }
+}
